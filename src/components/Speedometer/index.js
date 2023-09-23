4dd1e81795f48b74d68704d72ee9db1d547c80e4
MajojuUsharani <128362822+MajojuUsharani@users.noreply.github.com>
2023-09-23 17:15:23 +0530

    Update index.js

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -29,7 +29,7 @@ class Speedometer extends Component {
         />
         <h1 className="heading">Speed is {speed}mph</h1>
         <p className="description">Min Limit is 0mph, Max Limit is 200mph</p>
-        <div className="btn-button">
+        <div>
           <button
             className="accelerate-btn"
             type="button"
